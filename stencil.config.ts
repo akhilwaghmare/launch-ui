3896926971630ac2c725814368db618a5582af25
Akhil Waghmare <akhilwaghmare@gmail.com>
2020-09-01 12:20:38 -0400

    Set up CSS variables for colors

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,13 +3,14 @@ import { Config } from '@stencil/core';
 export const config: Config = {
   namespace: 'launch-ui',
   taskQueue: 'async',
+  globalStyle: 'src/global/variables.css',
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle'
+      type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme',
